cbdff92a93146edda04bdb8a4a2123ad95ecf86d
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2023-12-15 11:53:29 -0500

    Rama Recalde Salazar

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
-
+/* FRFRF */
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
