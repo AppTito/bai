aa1ebebe0d0610e4cc3bf3d2da1f3cd95099fbb3
Ricardo Zaldumbide <104150751+rszaldumbide@users.noreply.github.com>
2023-12-16 03:34:14 -0500

    add nextui, finish login, config twind

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,97 +1,147 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect } from "react";
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import Checkbox from "@/Components/Checkbox";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import imagenGif from "@/imgs/imagenGif.gif";
+
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+
+import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: false,
     });
 
     useEffect(() => {
         return () => {
-            reset('password');
+            reset("password");
         };
     }, []);
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('login'));
+        post(route("login"));
     };
 
     return (
-        <GuestLayout>
-            <Head title="Log in" />
+        <div className="bg-[#F2F2F2]">
+            <div className="container mx-auto">
+                <div className="flex flex-col md:flex-row justify-around md:pb-10">
+                    <Card className="bg-white mx-auto md:w-[390px] md:h-[600px] h-screen md:rounded-2xl shadow-2xl shadow-gray-400 md:mb-0 lg:my-16 ">
+                        <CardHeader>
+                            <ApplicationLogo className="block h-28 w-auto mx-auto rounded-full mt-3" />
+                        </CardHeader>
+                        <CardBody>
+                            <h2 className="text-3xl text-center mb-5 font-semibold">
+                                Banco de Alimentos - Imbabura
+                            </h2>
+                            <form className="px-10 py-3" onSubmit={submit}>
+                                <div>
+                                    <InputLabel
+                                        htmlFor="email"
+                                        value="Correo/Usuario"
+                                    />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+                                    <TextInput
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        value={data.email}
+                                        className="mt-1 block w-full"
+                                        autoComplete="username"
+                                        isFocused={true}
+                                        onChange={(e) =>
+                                            setData("email", e.target.value)
+                                        }
+                                    />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+                                    <InputError
+                                        message={errors.email}
+                                        className="mt-2"
+                                    />
+                                </div>
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
+                                <div className="mt-4">
+                                    <InputLabel
+                                        htmlFor="password"
+                                        value="Contraseña"
+                                    />
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
+                                    <TextInput
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        value={data.password}
+                                        className="mt-1 block w-full"
+                                        autoComplete="current-password"
+                                        onChange={(e) =>
+                                            setData("password", e.target.value)
+                                        }
+                                    />
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                                    <InputError
+                                        message={errors.password}
+                                        className="mt-2"
+                                    />
+                                </div>
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
+                                <div className="block mt-4">
+                                    <label className="flex items-center">
+                                        <Checkbox
+                                            name="remember"
+                                            checked={data.remember}
+                                            className="text-[#00553f]"
+                                            onChange={(e) =>
+                                                setData(
+                                                    "remember",
+                                                    e.target.checked
+                                                )
+                                            }
+                                        />
+                                        <span className="ms-2 text-sm text-gray-600">
+                                            Recordarme
+                                        </span>
+                                    </label>
+                                </div>
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
+                                <div className="mt-4">
+                                    <PrimaryButton
+                                        className="w-full items-center justify-center bg-[#00553f] h-12"
+                                        disabled={processing}
+                                    >
+                                        Iniciar Sesión
+                                    </PrimaryButton>
+                                </div>
+                                <div className="mt-4 text-center">
+                                    {canResetPassword && (
+                                        <Link
+                                            href={route("password.request")}
+                                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        >
+                                            ¿Olvidaste la contraseña?
+                                        </Link>
+                                    )}
+                                </div>
+                            </form>
+                        </CardBody>
+                    </Card>
+                    {/* Imagen */}
+                    <div className="w-full md:w-[750px] mx-auto hidden md:block">
+                        <Image
+                            width={1000}
+                            alt="git de login"
+                            src="https://admin-panel-portfolio.netlify.app/static/media/Revenue-cuate.86ac51593ee251c24d99c2b0ddf39a6f.svg"
                         />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
+                    </div>
                 </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+            </div>
+        </div>
     );
 }
