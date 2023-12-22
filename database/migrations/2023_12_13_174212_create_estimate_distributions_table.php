ff89c964a6c441e8f3498025fec0dc7c57e57b89
Nataly <natalysalazar646@gmail.com>
2023-12-22 18:13:55 -0500

    Modificación de estimaciones
    Modificación de las tablas estimates y distributions, creación de la tabla produc_distributions

--- database/migrations/2023_12_13_174212_create_estimate_distributions_table.php
+++ database/migrations/2023_12_13_174212_create_estimate_distributions_table.php
@@ -4,13 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        /**
+         * Se utiliza product_distributions en lugar de esta tabla
+         */
         Schema::create('estimate_distributions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('estimate_id')->constrained();
