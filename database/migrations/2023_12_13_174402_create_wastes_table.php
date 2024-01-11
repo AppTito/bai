49241a3887e95b55982d3468ec7ae852e55d1a42
ajrodriguezp <153867415+ajrodriguezp@users.noreply.github.com>
2024-01-11 16:42:07 -0500

    Remove foreign key constraint for user_id in wastes table migration

--- database/migrations/2023_12_13_174402_create_wastes_table.php
+++ database/migrations/2023_12_13_174402_create_wastes_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('wastes', function (Blueprint $table) {
             $table->id();
             $table->string('item');
-            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
