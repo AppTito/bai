e9997b40c7c31db47015915c30125c6d4d93e928
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-01-12 23:31:03 -0500

    Refactor TableRowControl and TableTheadControl components

--- resources/js/Components/TableTheadControl.jsx
+++ resources/js/Components/TableTheadControl.jsx
@@ -1,15 +1,35 @@
+// TableHeaderRow.jsx
 import React from "react";
+import { Icon } from "@iconify/react";
+import "../../css/TableHeaderRow.css"; 
+
+// Función de mapeo para convertir los nombres en íconos
+const mapIcon = (iconName) => {
+  // Lógica para mapear los nombres a los íconos reales
+  // Puedes personalizar esto según tus necesidades
+  switch (iconName) {
+    case "icon:emoji-name":
+      return "emojione:smile";
+    case "icon:percentage":
+      return "ic:baseline-percentage";
+    // Agrega más casos según sea necesario
+    default:
+      return null;
+  }
+};
 
 const TableHeaderRow = ({ columnNames }) => {
-    return (
-        <tr className="bg-primary">
-            {columnNames.map((name, index) => (
-                <th key={index} className="p-2 border">
-                    {name}
-                </th>
-            ))}
-        </tr>
-    );
+  return (
+    <tr className="bg-primary">
+      {columnNames.map((column, index) => (
+        <th key={index} className="p-2 border">
+          <span className="icon-container" title={Object.keys(column)[0]}>
+            <Icon icon={mapIcon(column[Object.keys(column)[0]])} />
+          </span>
+        </th>
+      ))}
+    </tr>
+  );
 };
 
 export default TableHeaderRow;
