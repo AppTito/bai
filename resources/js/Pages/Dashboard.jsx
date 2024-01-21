fa51efc06d2b1e5acf14b2e80c21f5220447c107
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-01-20 19:08:23 -0500

    Fix import formatting and add dashboard gif

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,8 +1,8 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import {Head, usePage} from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, usePage } from "@inertiajs/react";
 
 export default function Dashboard(props) {
-    const { auth } = usePage().props
+    const { auth } = usePage().props;
 
     return (
         <AuthenticatedLayout user={auth.user}>
@@ -12,6 +12,13 @@ export default function Dashboard(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">Bienvenido!</div>
+                    </div>
+                    <br />
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        {/* video o gif dashboard*/}
+                        <div className="p-6 text-gray-900">
+                            <img src="https://media.giphy.com/media/3o7aD2FQDSVdppxIQg/giphy.gif" />
+                        </div>
                     </div>
                 </div>
             </div>
