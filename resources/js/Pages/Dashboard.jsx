767a314f5095c097b88837f5dd49fed73ba4c854
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-01-21 10:26:57 -0500

    Add GIF to Dashboard component

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, usePage } from "@inertiajs/react";
 import vid from "../../vids/food.mp4";
+import gif from "../../vids/Produce.gif";
 
 export default function Dashboard(props) {
     const { auth } = usePage().props;
@@ -11,20 +12,25 @@ export default function Dashboard(props) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Bienvenido!</div>
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900">
+                        <h2 className="text-2xl font-bold mb-4">Bienvenido!</h2>
+                        {/* Additional content can be added here */}
                     </div>
                     <br />
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        {/*gif dashboard*/}
-                        <video
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg text-center">
+                        {/* Video or GIF dashboard */}
+                        {/* Video */}
+                        {/* <video
                             className="w-full h-full object-cover"
                             autoPlay
                             loop
                             muted
                         >
                             <source src={vid} type="video/mp4" />
-                        </video>
+                        </video> */}
+                        
+                        {/* GIF */}
+                        <img className="mx-auto mt-4" src={gif} alt="gif" />
                     </div>
                 </div>
             </div>
