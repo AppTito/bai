091efb51a29e7458e5608df8a5a3536a51ca7529
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-01-23 19:20:37 -0500

    Fix video import and update dashboard heading

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, usePage } from "@inertiajs/react";
-import vid from "../../vids/food.mp4";
-import gif from "../../vids/Produce.gif";
+/* import vid from "../../vids/food.mp4";
+ */import gif from "../../vids/Produce.gif";
 
 export default function Dashboard(props) {
     const { auth } = usePage().props;
@@ -13,7 +13,7 @@ export default function Dashboard(props) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 text-gray-900">
-                        <h2 className="text-2xl font-bold mb-4">Bienvenido!</h2>
+                        <h2 className="text-2xl font-bold mb-4 text-green-800">Bienvenid@! al Banco de Alimentos Imbabura</h2>
                         {/* Additional content can be added here */}
                     </div>
                     <br />
