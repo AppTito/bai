0efa279263c318539c73a6b3cab3ec25218642c5
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-01-25 00:31:24 -0500

    Refactor weight handling and remove unused code

--- resources/js/Pages/Operations/Control.jsx
+++ resources/js/Pages/Operations/Control.jsx
@@ -6,54 +6,6 @@ import TableRowControl from "@/Components/Operations/TableRowControl";
 export default function Index(props) {
     const { categories, donors_id, date, waste } = usePage().props;
 
-    // Estados para los valores de peso
-    const [pesoGavetas, setPesoGavetas] = useState("");
-    const [pesoProcesado, setPesoProcesado] = useState("");
-    const [pesoTotal, setPesoTotal] = useState("");
-
-    // Función para manejar el cambio en el input de peso Gavetas
-    const handlePesoGavetasChange = (event) => {
-        const value = event.target.value;
-        setPesoGavetas(value);
-
-        // Calcular el Peso Total: gaveta - procesado
-        const total = parseFloat(value) - parseFloat(pesoProcesado);
-        setPesoTotal(isNaN(total) ? "" : total);
-    };
-
-    // Función para manejar el cambio en el input de peso Procesado
-    const handlePesoProcesadoChange = (event) => {
-        const value = event.target.value;
-        setPesoProcesado(value);
-
-
-    };
-
-    const handlePesoTotalChange = (event) => {
-        // Calcular el Peso Total: gaveta - procesado
-        const total = parseFloat(pesoGavetas) - parseFloat(value);
-        setPesoTotal(isNaN(total) ? "" : total);
-    };
-
-    // Función para manejar el cambio en los valores de la tabla
-    const handleTableChange = (newAllCellValues) => {
-        // Actualizar el estado con todos los valores de las celdas
-        const columnSums = Array(7).fill(0);
-        newAllCellValues.forEach((row) => {
-            row.forEach((value, index) => {
-                columnSums[index] += value;
-            });
-        });
-
-        // Actualizar los estados solo si hay cambios
-        setPesoGavetas(newAllCellValues[17][6].toString());
-        setPesoProcesado(newAllCellValues[17][0].toString());
-        const valueGavetas = parseFloat(pesoGavetas);
-        const valueProcesado = parseFloat(pesoProcesado);
-        const total = valueGavetas - valueProcesado;
-        setPesoTotal(total.toString());
-    };
-
     return (
         <AuthenticatedLayout user={props.auth.user} errors={props.errors}>
             <Head title="Control" />
@@ -70,69 +22,6 @@ export default function Index(props) {
                             </Link>
                         </div>
 
-                        {/* Pesos */}
-                        <h2 className="text-2xl font-bold text-start text-green-700 p-2">
-                            Donante: {donors_id.name}
-                        </h2>
-                        <h2 className="text-2xl font-bold text-start text-green-700 p-2">
-                            Fecha: {date}
-                        </h2>
-                        {/* Inputs de pesos */}
-                        <div className="flex flex-wrap mb-4">
-                            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <label
-                                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                    htmlFor="grid-peso-total"
-                                >
-                                    Peso Total
-                                </label>
-                                <input
-                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                    id="grid-peso-total"
-                                    type="number"
-                                    placeholder="Peso Total"
-                                    min={0}
-                                    value={pesoGavetas}
-                                    onChange={handlePesoGavetasChange}
-                                />
-                            </div>
-                            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <label
-                                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                    htmlFor="grid-peso-recuperado"
-                                >
-                                    Peso Recuperado
-                                </label>
-                                <input
-                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                    id="grid-peso-recuperado"
-                                    type="number"
-                                    placeholder="Peso Recuperado"
-                                    min={0}
-                                    contentEditable={false}
-                                    value={pesoProcesado}
-                                    onChange={handlePesoProcesadoChange}
-                                />
-                            </div>
-                            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <label
-                                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                    htmlFor="grid-peso-final"
-                                >
-                                    Peso Final
-                                </label>
-                                <input
-                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                    id="grid-peso-final"
-                                    type="number"
-                                    placeholder="Peso Final"
-                                    min={0}
-                                    contentEditable={false}
-                                    value={pesoTotal}
-                                    onChange={handlePesoTotalChange}
-                                />
-                            </div>
-                        </div>
                         {/* Tabla para llenar con los pesos */}
                         <h2 className="text-2xl font-bold text-start text-green-700 p-2">
                             Control:
